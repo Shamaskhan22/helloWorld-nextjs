7882952ce59bd78ba50547f8f483486223d07a39
shamas <shamaskhan2003@gmail.com>
2024-10-04 11:03:16 +0500

    hello world printed

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+return(
+   <div>
+  <h1>"Hello World!"</h1>
+  <h2>"Hellow World?"</h2>
+  <p>this is a paragraph</p>
+  <br></br>
+  <h1>"Hello World!"</h1>
+  <h2>"Hellow World?"</h2>
+  <p>this is a paragraph</p>
+</div>
+);
+}
