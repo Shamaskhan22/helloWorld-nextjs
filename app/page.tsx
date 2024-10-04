fb0bfa0fcf5e41a36b282a762fd1177a7bd7e7a5
shamas <shamaskhan2003@gmail.com>
2024-10-04 11:27:25 +0500

    hello world fixed

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,7 @@ export default function Home() {
 return(
    <div>
   <h1>"Hello World!"</h1>
-  <h2>"Hellow World?"</h2>
-  <p>this is a paragraph</p>
-  <br></br>
-  <h1>"Hello World!"</h1>
-  <h2>"Hellow World?"</h2>
-  <p>this is a paragraph</p>
+  
 </div>
 );
 }
